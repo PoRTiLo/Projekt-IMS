04ad3ef48f7598ec9626711e5bec176ed284c169
PoRTiLo <portilo@seznam.cz>
2010-12-08 12:20:30 +0100

    uklizeno

--- main.cpp
+++ main.cpp
@@ -1,8 +1,44 @@
 #include "PetriSim.h"
+
+#include "calendar.h"
 int main(int argc,const char* argv[])
 {
 
-	SCPlace p[5];
+	SCCalendar* cal = new SCCalendar();
+	SCPlace p;
+//	CalElement el = new CalElement(p, 2.0);
+	cal->insert(p, 2.0);
+		cout << "cal->head->cont->time"<<cal->getFirst()->cont->time << endl;
+		cout << "cal->head->pre->time"<<cal->getFirst()->pre->time << endl;
+		cout << "cal_fisrt->->time"<<cal->getFirst()->time << endl;
+		cout << "pocet"<<cal->getCount()<<endl;
+	cal->insert(p, 3.0);
+		cout << "cal->head->cont->time"<<cal->getFirst()->cont->time << endl;
+		cout << "cal->head->pre->time"<<cal->getFirst()->pre->time << endl;
+		cout << "cal_fisrt->->time"<<cal->getFirst()->time << endl;
+		cout << "pocet"<<cal->getCount()<<endl;
+	cal->insert(p, 1.0);
+		cout << "cal->head->cont->time"<<cal->getFirst()->cont->time << endl;
+		cout << "cal->head->pre->time"<<cal->getFirst()->pre->time << endl;
+		cout << "cal_fisrt->->time"<<cal->getFirst()->time << endl;
+		cout << "pocet"<<cal->getCount()<<endl;
+cout<<"vlozena trojka melo by byt 2, 3";	cal->show();
+SCPlace* place;
+SCCalendarUnit unit(place,5.0);
+cal->insert(unit);
+		cout << "cal->head->cont->time"<<cal->getFirst()->cont->time << endl;
+		cout << "cal->head->pre->time"<<cal->getFirst()->pre->time << endl;
+		cout << "cal_fisrt->->time"<<cal->getFirst()->time << endl;
+		cout << "pocet"<<cal->getCount()<<endl;
+		cout << "end" << cal->getLast()->time <<endl;
+		cal->show();
+		cout<<"....p...."<<cal->getNextUnit().time<<endl;
+		cal->remove();
+		cout<<"show po delete"<<endl;
+		cal->show();
+
+//	cal->insert(p[2], 3.4);
+/*	SCPlace p[5];
 	SCDirectedArc a[8];
 	SCTransition t[3];
 	p[0].SetArgStartVal(1);
@@ -17,6 +53,7 @@ int main(int argc,const char* argv[])
 	TrToPl(&t[2],&p[1],&a[6]);
 	TrToPl(&t[2],&p[4],&a[7]);
 
-	Run();*/
+	Run();
+	*/
 	return 0;
 }
